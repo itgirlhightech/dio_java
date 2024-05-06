cfeb08a3988e2a28d6c7c5441f4d957e98869923
Evilyn Feitosa <evy123feitosa@gmail.com>
2024-05-06 14:16:28 -0300

    commit para meu github dio_java.

--- meu.primeiro.projeto.java/about.me/src/About/me.java
+++ meu.primeiro.projeto.java/about.me/src/About/me.java
@@ -1,17 +1,29 @@
 package About;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class me {
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        String nome = args [0];
-        String sobrenome = args [1];
-        int idade = Integer.valueOf(args[2]);
-        double altura = Double.valueOf(args[3]);
 
-        System.out.println("olá, meu nome é  " + nome + " " + sobrenome);
-        System.out.println("tenho  " + idade + "anos");
-        System.out.println("minha altura é  " + altura + "cm ");
+        System.out.println("digite seu nome:  ");
+        String nome = scanner.next();
 
+        System.out.println("digite seu sobrenome: ");
+        String sobrenome = scanner.next();
+
+        System.out.println("digite sua idade: ");
+        int idade = scanner.nextInt();
+
+        System.out.println("digite sua altura: ");
+        double altura = scanner.nextDouble();
+
+
+        System.out.println("Olá, me chamo " + nome + "  " + sobrenome);
+        System.out.println("Tenho " + idade + " anos ");
+        System.out.println("Minha altura é  " + altura + "cm ");
     }
 
 }
